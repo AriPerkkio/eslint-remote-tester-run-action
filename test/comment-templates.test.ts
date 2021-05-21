259a632ccbe2583275db57de48655ea2da8e1195
Ari Perkkiö <ari.perkkio@gmail.com>
2021-05-21 15:16:27 +0000

    fix: support non-JSON values in eslint-remote-tester.config.js
    - Handle user provided configuration file as javascript module instead of as JSON internally

--- test/comment-templates.test.ts
+++ test/comment-templates.test.ts
@@ -126,15 +126,15 @@ describe('ERROR_TEMPLATE', () => {
             \`\`\`
             Error: mock error
                 at Object.<anonymous> (<removed>/test/utils.ts:2:26)
-                at Runtime._execModule (<removed>/node_modules/jest-runtime/build/index.js:1299:24)
-                at Runtime._loadModule (<removed>/node_modules/jest-runtime/build/index.js:898:12)
-                at Runtime.requireModule (<removed>/node_modules/jest-runtime/build/index.js:746:10)
-                at Runtime.requireModuleOrMock (<removed>/node_modules/jest-runtime/build/index.js:919:21)
+                at Runtime._execModule (<removed>/node_modules/jest-runtime/build/index.js:867:68)
+                at Runtime._loadModule (<removed>/node_modules/jest-runtime/build/index.js:577:12)
+                at Runtime.requireModule (<removed>/node_modules/jest-runtime/build/index.js:433:10)
+                at Runtime.requireModuleOrMock (<removed>/node_modules/jest-runtime/build/index.js:598:21)
                 at Object.<anonymous> (<removed>/test/comment-templates.test.ts:4:1)
-                at Runtime._execModule (<removed>/node_modules/jest-runtime/build/index.js:1299:24)
-                at Runtime._loadModule (<removed>/node_modules/jest-runtime/build/index.js:898:12)
-                at Runtime.requireModule (<removed>/node_modules/jest-runtime/build/index.js:746:10)
-                at jasmine2 (<removed>/node_modules/jest-jasmine2/build/index.js:230:13)
+                at Runtime._execModule (<removed>/node_modules/jest-runtime/build/index.js:867:68)
+                at Runtime._loadModule (<removed>/node_modules/jest-runtime/build/index.js:577:12)
+                at Runtime.requireModule (<removed>/node_modules/jest-runtime/build/index.js:433:10)
+                at <removed>/node_modules/jest-jasmine2/build/index.js:202:13
             \`\`\`
 
             </details>
--- test/run-tester.test.ts
+++ test/run-tester.test.ts
@@ -1,9 +1,11 @@
 import fs from 'fs';
+import { resolve } from 'path';
 import { exec } from '@actions/exec';
 
 import runTester from '../src/run-tester';
 import { ESLINT_REMOTE_TESTER_BIN } from '../src/peer-dependencies';
 import { sanitizeStackTrace } from './utils';
+import { Config } from 'eslint-remote-tester/dist/exports-for-compare-action';
 
 const EXPECTED_RUN_CONFIG = './eslint-remote-tester-compare-internal.config.js';
 const CONFIG = './test/eslint-remote-tester.config.js';
@@ -14,13 +16,18 @@ const CONFIG_WITH_ON_COMPLETE =
 jest.mock('@actions/exec', () => ({ exec: jest.fn() }));
 
 function readRunConfig() {
-    const content = fs.readFileSync(EXPECTED_RUN_CONFIG, 'utf8');
+    let content: Config;
 
-    // require doesn't work here even when combined with jest.resetModules
-    return eval(content);
+    jest.isolateModules(() => {
+        content = require(resolve(EXPECTED_RUN_CONFIG));
+    });
+
+    return content!;
 }
 
 function cleanup() {
+    jest.resetModules();
+
     if (fs.existsSync(EXPECTED_RUN_CONFIG)) {
         fs.unlinkSync(EXPECTED_RUN_CONFIG);
     }
@@ -46,37 +53,23 @@ describe('run-tester', () => {
     });
 
     test('throws if given configuration does not exist', () => {
-        return expect(() =>
-            runTester('./non-existing-config')
-        ).rejects.toThrowError(
+        return expect(runTester('./non-existing-config')).rejects.toThrowError(
             /(?=.*Unable to find eslint-remote-tester config with path)(?=.*non-existing-config)/
         );
-    });
-
-    test('onComplete is valid if users configuration contains no onComplete', async () => {
-        await runTester(CONFIG);
-
-        expect(sanitizeStackTrace(readRunConfig().onComplete.toString()))
-            .toMatchInlineSnapshot(`
-            "async function onComplete(results, comparisonResults) {
-                    fs.writeFileSync('/tmp/results.json', JSON.stringify(results || []));
-
-                    // User provided onComplete is injected here if present
-                    // No onComplete detected
-                }"
-        `);
     });
 
     test('uses onComplete from users configuration', async () => {
         await runTester(CONFIG_WITH_ON_COMPLETE);
 
-        expect(sanitizeStackTrace(readRunConfig().onComplete.toString()))
+        expect(sanitizeStackTrace(readRunConfig().onComplete!.toString()))
             .toMatchInlineSnapshot(`
             "async function onComplete(results, comparisonResults) {
+                    // Write results to cache
                     fs.writeFileSync('/tmp/results.json', JSON.stringify(results || []));
 
-                    // User provided onComplete is injected here if present
-                    await require('<removed>/test/eslint-remote-tester.config.onComplete.js').onComplete(results, comparisonResults);
+                    if(usersConfig.onComplete) {
+                        await usersConfig.onComplete(results, comparisonResults);
+                    }
                 }"
         `);
     });
@@ -97,12 +90,12 @@ describe('run-tester', () => {
         const consolelog = console.log;
         console.log = jest.fn();
 
-        return expect(() => runTester(INVALID_CONFIG))
+        return expect(runTester(INVALID_CONFIG))
             .rejects.toThrowErrorMatchingInlineSnapshot(
                 `
-                    "Configuration validation errors:
-                    - Missing repositories."
-                `
+"Configuration validation errors:
+- Missing repositories."
+`
             )
             .then(() => (console.log = consolelog));
     });
